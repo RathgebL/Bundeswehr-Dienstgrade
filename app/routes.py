88d6865a285cb303dbce18423bc2a7f0aa555f4d
RathgebL <lennart.rathgeb@gmail.com>
2025-10-25 13:13:18 +0200

    add start page

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template, request
 from .models import Rank
+import random
 
 bp = Blueprint("main", __name__)
 
+# Startseite
 @bp.route("/")
+def index():
+    return render_template("index.html")
+
+# Tabelle
+@bp.route("/ranks")
 def home():
     # Optional: Dropdown-Filter (z. B. ?branch=Heer)
     branch = request.args.get("branch")
@@ -15,3 +22,21 @@ def home():
     branches = ["Heer", "Luftwaffe", "Marine"]
 
     return render_template("ranks.html", ranks=ranks, branches=branches, selected_branch=branch)
+
+# Quiz
+@bp.route("/quiz")
+def quiz():
+    # alle Dienstgrade laden
+    ranks = Rank.query.all()
+
+    # 1 richtigen zufällig wählen
+    correct = random.choice(ranks)
+
+    # 3 falsche Antworten (ohne den richtigen)
+    wrong = random.sample([r for r in ranks if r.id != correct.id], 3)
+
+    # Antworten mischen
+    options = [correct] + wrong
+    random.shuffle(options)
+
+    return render_template("quiz.html", correct=correct, options=options)
